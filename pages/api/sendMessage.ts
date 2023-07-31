8c12be30eec941d5406abf30ae581090e0869b2b
Nego <61694826+kardespro@users.noreply.github.com>
2023-08-01 01:00:24 +0400

    Update sendMessage.ts

--- pages/api/sendMessage.ts
+++ pages/api/sendMessage.ts
@@ -3,7 +3,7 @@ import { Rcon } from 'rcon-client';
 
 interface QueryParams {
   host: string;
-  port: string;
+  port: number;
   password: string;
   message: string;
 }
@@ -17,25 +17,32 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  const { host, port, password, message }: QueryParams = req.query;
+  const { host, port, password, message } = req.query;
   if (!host || !port || !password || !message) {
     return res.status(400).json({ status: 400, message: 'Missing parameters' });
   }
 
-  const parsedPort = parseInt(port, 10);
+  const parsedPort = parseInt(port as string, 10);
   if (isNaN(parsedPort)) {
     return res.status(400).json({ status: 400, message: 'Invalid port number' });
   }
 
-  const rcon = new Rcon({
-    host,
+  const queryParams: QueryParams = {
+    host: host as string,
     port: parsedPort,
-    password,
+    password: password as string,
+    message: message as string,
+  };
+
+  const rcon = new Rcon({
+    host: queryParams.host,
+    port: queryParams.port,
+    password: queryParams.password,
   });
 
   try {
     await rcon.connect();
-    const response = await rcon.send(message);
+    const response = await rcon.send(queryParams.message);
     res.status(200).json({ status: 200, message: response });
   } catch (error: any) {
     res.status(500).json({ status: 500, message: 'An error occurred while connecting' });
