91d4779bb9f4c2bd26a78e0b4542481f9ad2be0c
Nego <61694826+kardespro@users.noreply.github.com>
2023-08-01 00:50:31 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  generateBuildId: async () => { 
+    return 'controlmc' 
+  },
 }
 
 module.exports = nextConfig
